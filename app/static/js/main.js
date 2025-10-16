7991d14bbd7e937b79f9c37074c394bec280e047
D221 <dailycast@proton.me>
2025-10-16 16:06:00 +0300

    style(youtube): center embed and adjust aspect ratio for responsiveness

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -428,8 +428,6 @@ document.addEventListener("DOMContentLoaded", () => {
 					return;
 				}
 				const iframe = document.createElement("iframe");
-				iframe.width = "560";
-				iframe.height = "315";
 				iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
 				iframe.frameBorder = "0";
 				iframe.allow =
