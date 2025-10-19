b0df73f778cfc46a0bbda0f50dc172ef4e34b97d
D221 <dailycast@proton.me>
2025-10-19 20:41:23 +0300

    docs(run): remove duplicate confusing startup information

--- run.sh
+++ run.sh
@@ -6,6 +6,4 @@ echo "Activating virtual environment and installing dependencies..."
 source venv/bin/activate
 pip install -r requirements.txt
 
-echo "Starting Vocaloid Rater on http://localhost:8000"
-echo "Press Ctrl+C to stop the server."
 uvicorn app.main:app --host localhost --port 8000
