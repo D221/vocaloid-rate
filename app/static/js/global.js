2ed1b284f1a00ea25a0058a2c19d8f71d159dfb4
D221 <dailycast@proton.me>
2026-02-13 09:25:03 +0200

    fix(deploy): fix service worker scope and registration path

--- app/static/js/global.js
+++ app/static/js/global.js
@@ -44,7 +44,7 @@ window._ = function (key, ...args) {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/static/sw.js", { scope: "/" })
+      .register("/sw.js", { scope: "/" })
       .catch((err) => {
         console.error("ServiceWorker registration failed: ", err);
       });
